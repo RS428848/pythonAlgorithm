002deb5dcae1ac819db323312a6b6b8760c8e8e3
Robert Song <rsong1617@gmail.com>
2019-03-11 21:18:41 -0400

    ad

--- sequenceBinary.py
+++ sequenceBinary.py
@@ -1,3 +1,4 @@
+
 #Find the index of 0 to be replaced with 1 to get longest continuous sequence of 1s in a binary array.
 #Returns index of 0 to be replaced with 1 to get longest continuous sequence of 1s. If there is no 0 in array, then it returns -1.
     #e.g.
@@ -41,4 +42,7 @@ finalAnswer.sort()
 finalAnswer.reverse()
 
 print (totalCount.get(finalAnswer[0]))
+
+
+
 
